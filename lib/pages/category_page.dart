7e2c982251d49bfb328610ea9ca7772074bd46cc
slyjs <danfeng1114@163.com>
2019-06-20 21:42:54 +0800

    该听31节课了

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -1,11 +1,16 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_shop/provide/child_category.dart';
+import 'package:flutter_shop/model/categoryGoodsList.dart' as prefix0;
+import '../provide/child_category.dart';
 import '../config/service_url.dart';
 import '../service/service_method.dart';
 import 'dart:convert';
 import '../model/category.dart';
+import '../model/categoryGoodsList.dart';
 import 'package:provide/provide.dart';
+import '../config/service_url.dart';
+import 'dart:async';
 
 class CategoryPage extends StatefulWidget {
   CategoryPage({Key key}) : super(key: key);
@@ -30,7 +35,10 @@ class _CategoryPageState extends State<CategoryPage> {
         children: <Widget>[
           LeftCategoryNav(),
           Column(
-            children: <Widget>[RightCategoryNav()],
+            children: <Widget>[
+              RightCategoryNav(),
+              CategoryGoodsList(),
+            ],
           )
         ],
       ),
@@ -49,6 +57,7 @@ class LeftCategoryNav extends StatefulWidget {
 class _LeftCategoryNavState extends State<LeftCategoryNav> {
   List list = [];
   var listIndex = 0;
+
   @override
   void initState() {
     _getCategory();
@@ -72,17 +81,34 @@ class _LeftCategoryNavState extends State<LeftCategoryNav> {
 
 //获取左侧菜单数据
   void _getCategory() async {
-    await request(GET_CATEGORY).then((val) {
-      var data = json.decode(val.toString());
-      print(data);
-      CategoryModel category = CategoryModel.fromJson(data);
-      setState(() {
-        list = category.data;
-      });
+    Future.delayed(
+      new Duration(seconds: 5),
+      () {},
+    ).then(
+      (data) {
+        //设置一个定时器，延时更改数据
+        var data = json.decode(getCategoryData.toString());
+        CategoryModel category = CategoryModel.fromJson(data);
+        setState(() {
+          list = category.data;
+        });
 
-      Provide.value<ChildCategory>(context)
-          .getChildCategory(list[0].bxMallSubDto);
-    });
+        Provide.value<ChildCategory>(context)
+            .getChildCategory(list[0].bxMallSubDto);
+      },
+    );
+
+    // await request(GET_CATEGORY).then((val) {
+    //   var data = json.decode(val.toString());
+    //   print('获取左侧的菜单数据接口信息》》》》》》》》${data}');
+    //   CategoryModel category = CategoryModel.fromJson(data);
+    //   setState(() {
+    //     list = category.data;
+    //   });
+
+    //   Provide.value<ChildCategory>(context)
+    //       .getChildCategory(list[0].bxMallSubDto);
+    // });
   }
 
 //左侧菜单的子控件
@@ -161,4 +187,126 @@ Widget _rightInkWell(BxMallSubDto item) {
       ),
     ),
   );
+}
+
+///右侧商品列表控件
+class CategoryGoodsList extends StatefulWidget {
+  CategoryGoodsList({Key key}) : super(key: key);
+
+  _CategoryGoodsListState createState() => _CategoryGoodsListState();
+}
+
+///右侧商品列表控件
+class _CategoryGoodsListState extends State<CategoryGoodsList> {
+  List<CategoryListData> list = [];
+  @override
+  void initState() {
+    _getGoodList();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //采用listView控件实现
+    return Container(
+      width: ScreenUtil().setWidth(560),
+      height: ScreenUtil().setHeight(900),
+      child: ListView.builder(
+        itemCount: list.length,
+        itemBuilder: (context, index) {
+          return _listWidget(index);
+        },
+      ),
+    );
+  }
+
+  void _getGoodList() async {
+    var data = {
+      'categoryId': '4',
+      'categorySubId': '',
+      'page': 1,
+    };
+    await request(GET_MALL_GOODS, formData: data).then((val) {
+      var data = json.decode(val.toString());
+      CategoryGoodsListModel goodsList = CategoryGoodsListModel.fromJson(data);
+      setState(() {
+        list = goodsList.data;
+      });
+      print("商品分类列表数据》》》》》》》${data}");
+    });
+  }
+
+  Widget _listWidget(index) {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        padding: EdgeInsets.only(top: 5, bottom: 5),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            bottom: BorderSide(
+              width: 1.0,
+              color: Colors.black12,
+            ),
+          ),
+        ),
+        child: Row(
+          children: <Widget>[
+            _goodsImage(index),
+            Column(
+              children: <Widget>[
+                _goodsName(index),
+                _goodsPrice(index),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  //商品图片
+  Widget _goodsImage(index) {
+    return Container(
+      width: ScreenUtil().setWidth(200),
+      child: Image.network(list[index].image),
+    );
+  }
+
+//商品名称
+  Widget _goodsName(index) {
+    return Container(
+      padding: EdgeInsets.all(5),
+      width: ScreenUtil().setWidth(360),
+      child: Text(
+        list[index].goodsName,
+        maxLines: 2,
+        overflow: TextOverflow.ellipsis,
+        style: TextStyle(fontSize: ScreenUtil().setSp(26)),
+      ),
+    );
+  }
+
+  Widget _goodsPrice(index) {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      width: ScreenUtil().setWidth(360),
+      child: Row(
+        children: <Widget>[
+          Text(
+            '���格：￥${list[index].presentPrice}',
+            style:
+                TextStyle(color: Colors.pink, fontSize: ScreenUtil().setSp(30)),
+          ),
+          Text(
+            '￥${list[index].oriPrice}',
+            style: TextStyle(
+              color: Colors.black26,
+              decoration: TextDecoration.lineThrough,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
--- lib/service/service_method.dart
+++ lib/service/service_method.dart
@@ -1,23 +1,27 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'dart:async';
 import 'dart:io';
 import '../config/service_url.dart';
-
+import '../config/httpHeaders.dart';
 
 Future request(rqUrl, {formData}) async {
   try {
-    print('开始获取首页数据...........rqurl==' + rqUrl);
+    print('开始获取数据...........rqurl==' + rqUrl);
     Response response;
     Dio dio = new Dio();
+    // dio.options.headers = HttpHeaders;
     dio.options.contentType =
-        ContentType.parse("application/x-www-form-urlencoded");
-        
-    if(rqUrl==HOMEPAGE_CONTENT) {
-      response = await dio.get(servicePath[rqUrl] +
-          "?lon=115.075234375&lat=35.776455078125");
+        ContentType.parse("application/x-www-form-urlencoded;charset=UTF-8");
+
+    if (rqUrl == HOMEPAGE_CONTENT) {
+      response = await dio
+          .get(servicePath[rqUrl] + "?lon=115.075234375&lat=35.776455078125");
+    } else if (formData == null) {
+      response = await dio.post(servicePath[rqUrl]);
     } else {
-      response =
-          await dio.post(servicePath[rqUrl], data: formData);//homePageBelowContent
+      response = await dio.post(servicePath[rqUrl], data: formData);
     }
     if (response.statusCode == 200) {
       return response.data;
